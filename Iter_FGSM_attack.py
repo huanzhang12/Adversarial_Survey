b9b5a2b079c600213fadb5a4d0588609bd5db605
YupengGao <yupeng.gao@outlook.com>
2017-09-11 23:14:12 -0400

    update on the iter_fgsm_attack.py

--- Iter_FGSM_attack.py
+++ Iter_FGSM_attack.py
@@ -4,7 +4,8 @@ import numpy as np
 import time
 
 class Iter_FGSM:
-    def __init__(self, sess, model, targeted=True, batch_size=1):
+    def __init__(self, sess, model, eps, eps_iter, iter_num, use_log = True,targeted=True, batch_size=1):
+        
         """
         The implementation of Ian Goodfellow's FGSM attack.
         Returns adversarial examples for the supplied model.
@@ -15,55 +16,53 @@ class Iter_FGSM:
         image_size, num_channels, num_labels = model.image_size, model.num_channels, model.num_labels
         self.sess = sess
         self.model = model
-        self.epsilon = tf.placeholder(tf.float32)
         self.batch_size = batch_size
-
+        self.eps = eps
+        self.eps_iter = eps_iter
+        self.iter_num = iter_num
         shape = (batch_size,image_size,image_size,num_channels)
 
         self.x = tf.Variable(np.zeros(shape), dtype=tf.float32)
         self.y = tf.Variable(np.zeros((batch_size,num_labels)), dtype=tf.float32)
+        self.eta = tf.Variable(np.zeros(shape), dtype=tf.float32)
         self.logits = self.model.predict(self.x)
         #Generate the gradient of the loss function.
-        # self.adv_loss = K.categorical_crossentropy(self.logits, self.y, from_logits=True)
+        self.y = self.y / tf.reduce_sum(self.y, 1, keep_dims=True)
+        #Generate the gradient of the loss function.
+        #decide whether use logits or softmax
+        if use_log:#use logits
+            op = self.logits.op
+            if "softmax" in str(op).lower():
+                self.logits, = op.inputs
+            else:
+                self.logits = self.logits
+        #else:
+            #use softmax
+
+
         self.adv_loss = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.y)
         if targeted:
             self.adv_loss = -self.adv_loss
-        #grad = K.gradients(self.adv_loss, x)
-        # self.grad = K.gradients(self.adv_loss, [self.x])[0]
         self.grad, = tf.gradients(self.adv_loss, self.x)
         # signed gradient
-        # self.normed_grad = K.sign(self.grad)
         self.normalized_grad = tf.sign(self.grad)
         self.normalized_grad = tf.stop_gradient(self.normalized_grad)
         # Multiply by constant epsilon
-        self.scaled_grad = self.epsilon * self.normalized_grad
+        self.scaled_grad = self.eps_iter * self.normalized_grad
         self.adv_x = self.x + self.scaled_grad
-        # Add perturbation to original example to obtain adversarial example
-        # self.adv_x = K.stop_gradient(self.x + self.scaled_grad)
 
-        #self.adv_x = K.clip(self.adv_x, -0.5, 0.5)
         self.adv_x = tf.clip_by_value(self.adv_x, -0.5, 0.5)
+        
+        return
 
-    def single_attack(self, imgs, targets, epsilon):
-        """
-        Perform the one-shot L_infinity FGSM attack on the given images for the given targets.
-        If self.targeted is true, then the targets represents the target labels.
-        If self.targeted is false, then targets are the original class labels.
-        """
-
-        #print("Perofming FGSM attack")
-
-        adv_x_concrete = self.sess.run(self.adv_x, feed_dict={self.x: imgs,
-                                                               self.y: targets,
-                                                               self.epsilon: epsilon})
-        #print("Done on the FGSM attack")
-
-        return adv_x_concrete
-
-    def attack(self, imgs, targets, epsilon, steps):
+    def attack(self, imgs, targets):
         print("performing iter_FGSM attack")
-        adv_x = imgs
-        for i in range(steps):
-            adv_x = self.single_attack(adv_x, targets, epsilon)
+        eta =  0
+        for i in range(self.iter_num):
+            adv_x = self.sess.run(self.adv_x, feed_dict={self.x: imgs + eta, self.y: targets })
+            eta = adv_x - imgs
+            eta = np.clip(eta, -self.eps, self.eps)
+        adv_x = eta + imgs
+        adv_x = np.clip(adv_x, -0.5,0.5)
         print("Done on the iter_FGSM attack")
         return adv_x
